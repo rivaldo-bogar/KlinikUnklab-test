473e0f9dcacfa64adf2f8f17a096396feb415fe0
rivaldo-bogar <androsbgr13@gmail.com>
2024-04-29 16:55:32 +0800

    realtime database save data konsultasi to database work

--- page/konsuldokter/konsul.js
+++ page/konsuldokter/konsul.js
@@ -1,22 +1,31 @@
+
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
+import { getDatabase,ref,set,get,child} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAejGuFe8kNlFxjXhFOrVK9Hg7Olu5Z4vc",
-    authDomain: "klinikunklab.firebaseapp.com",
-    databaseURL: "https://klinikunklab-default-rtdb.firebaseio.com",
-    projectId: "klinikunklab",
-    storageBucket: "klinikunklab.appspot.com",
-    messagingSenderId: "1078439843358",
-    appId: "1:1078439843358:web:f4f86c13a409f4ac7d400f",
-    measurementId: "G-2Q6G8VMXBS"
-  };
+  apiKey: "AIzaSyAejGuFe8kNlFxjXhFOrVK9Hg7Olu5Z4vc",
+  authDomain: "klinikunklab.firebaseapp.com",
+  databaseURL: "https://klinikunklab-default-rtdb.firebaseio.com",
+  projectId: "klinikunklab",
+  storageBucket: "klinikunklab.appspot.com",
+  messagingSenderId: "1078439843358",
+  appId: "1:1078439843358:web:f4f86c13a409f4ac7d400f",
+  measurementId: "G-2Q6G8VMXBS"
+   
+};
+ // Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const btnlog = document.getElementById('daftarkon');
 
-  btnlog.addEventListener('click',function(){
-   console.log("testing bug");
-  });
+
+  const db = getDatabase(app);
+  document.getElementById("daftarkon").addEventListener('click',function(e){
+    set(ref(db,'daftarkonsul/' + document.getElementById("nama").value),
+    {
+      username : document.getElementById("nama").value,
+      tanggal : document.getElementById("tanggal").value,
+      NamaDokter : document.getElementById("namadokter").value
+    })
+    alert("Login Success");
+  })
