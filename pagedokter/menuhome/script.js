e17d299be1955bd038b5f08b291d7ccb65f6b489
rivaldo-bogar <androsbgr13@gmail.com>
2024-04-29 15:34:52 +0800

    solve button track page

---
+++ pagedokter/menuhome/script.js
@@ -0,0 +1,15 @@
+document.getElementById('konsuldokter').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../jadwalpasien/";
+    console.log("codenya jalan boss");
+}
+document.getElementById('perubahanjadwal').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../Datapasien/";
+    console.log("codenya jalan boss");
+}
+document.getElementById('riwayatmedis').onclick = function() {
+    // Arahkan pengguna ke halaman berikutnya
+    window.location.href = "../riwayatmedis/";
+    console.log("codenya jalan boss");
+}
